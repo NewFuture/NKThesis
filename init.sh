17d70fc513342edb32b82ad24f58e9cc6d86ac4b
New Future <NewFuture@users.noreply.github.com>
2024-05-11 09:59:44 +0000

    fix fonts download

--- init.sh
+++ init.sh
@@ -5,11 +5,11 @@ sudo apt install -y texlive-latex-base latex-cjk-all texlive-latex-extra texmake
 # install ms fonts
 # echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true | sudo debconf-set-selections && sudo apt install -y ttf-mscorefonts-installer
 
-
-
-# install nkthesis fonts
-sudo wget https://github.com/dolbydu/font/raw/master/Serif/Times%20New%20Roman/Times.ttf  --directory-prefix /usr/share/fonts/opentype/nkthesis/
-sudo wget https://github.com/dolbydu/font/raw/master/unicode/FangSong.ttf --directory-prefix /usr/share/fonts/opentype/nkthesis/
-sudo wget https://github.com/dolbydu/font/raw/master/unicode/SimHei.ttf --directory-prefix /usr/share/fonts/opentype/nkthesis/
-sudo wget https://github.com/dolbydu/font/raw/master/unicode/SimSun.ttc --directory-prefix /usr/share/fonts/opentype/nkthesis/
-sudo wget https://github.com/dolbydu/font/raw/master/unicode/Kaiti.ttf /usr/share/fonts/opentype/nkthesis/
+# download nkthesis fonts
+sudo wget --directory-prefix /usr/share/fonts/opentype/nkthesis/ \
+ https://github.com/dolbydu/font/raw/master/Serif/Times%20New%20Roman/Times.ttf \
+ https://github.com/dolbydu/font/raw/master/unicode/FangSong.ttf \
+ https://github.com/dolbydu/font/raw/master/unicode/SimHei.ttf \
+ https://github.com/dolbydu/font/raw/master/unicode/SimSun.ttc \
+ https://github.com/dolbydu/font/raw/master/unicode/Kaiti.ttf \
+ -nv
