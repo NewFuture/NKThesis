602dd5942ac20ea7abce2cbc40e06d4c2e678c40
New Future <NewFuture@users.noreply.github.com>
2024-05-12 01:39:40 +0800

    reorder the tex files and editor settings (#38)

--- build.cmd
+++ build.cmd
@@ -2,10 +2,10 @@
     rm -rf *.aux *.log *.bbl *.blg *.bcf *.toc *.fls *.nav *.out *.snm *.fdb_latexmk *.vrb *.gz *.run.xml main-blx.bib tex/*.aux
 )&(
     (
-        xelatex -interaction=nonstopmode -halt-on-error main 
+        xelatex -interaction=nonstopmode -halt-on-error main.tex
     ) && biber main && (
-        xelatex -interaction=nonstopmode -halt-on-error main 
+        xelatex -interaction=nonstopmode -halt-on-error main.tex
     ) && (
-        xelatex -interaction=nonstopmode main
+        xelatex -interaction=nonstopmode main.tex
     )
 )
